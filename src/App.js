39a48b1aa054e27d53bd8b371d1f2e3d73089700
Darian Brown <browndarian011@gmail.com>
2023-04-08 13:10:53 -0400

    added component files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Contact from './Components/contact'
+// import Services from './Components/services'
+// import Projects from './Components/projects'
+import Nav from './Components/nav'
+import './App.css';
+import './index.css'
+
+export default function App () {
+  return(
+    <div>
+      <Contact/>
+      
+    </div>
+  )
+}
